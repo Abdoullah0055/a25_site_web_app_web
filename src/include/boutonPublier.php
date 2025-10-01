445b65c2169c85e3325f5d2318ccd4489f302f97
= <=>
2025-10-01 11:53:10 -0400

    premiere version du formulaire + page formulaire

--- src/include/boutonPublier.php
+++ src/include/boutonPublier.php
@@ -1,8 +1,7 @@
 <!-- Bouton Publier mon article -->
 <div class="max-w-6xl mx-auto px-6 py-8 text-center">
-    <button 
-        type="button" 
-        class="bg-silver text-blacklux font-semibold px-6 py-3 rounded-xl shadow hover:bg-anthracite hover:text-light transition">
+    <a href="publier.php"
+       class="bg-silver text-blacklux font-semibold px-6 py-3 rounded-xl shadow hover:bg-anthracite hover:text-light transition inline-block">
         Publier mon article
-    </button>
+    </a>
 </div>
