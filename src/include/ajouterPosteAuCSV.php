bd183d371c308d62f27fb8feb85f19a7f3cdea91
= <=>
2025-10-05 17:06:39 -0400

    Image placeholder incorporé quand un utilisateur envoi un post sans image.

--- src/include/ajouterPosteAuCSV.php
+++ src/include/ajouterPosteAuCSV.php
@@ -2,7 +2,6 @@
 // Récuperer les éléments du formulaire
 
 $cheminFichier = __DIR__ . "/../BD_CSV/informations_annonces.csv";
-$fichier = @fopen($cheminFichier, "a");
 
 // Vérifier si le fichier existe
 if (!file_exists($cheminFichier)) {
@@ -14,23 +13,29 @@ if (!is_writable($cheminFichier)) {
     exit("Erreur: Le fichier n'est pas accessible en écriture. Vérifiez les permissions.");
 }
 
-$titre = $description = $prix = $negociable = $image = $vendeur = "";
+$fichier = @fopen($cheminFichier, "a");
 
+$titre = $description = $prix = $negociable = $image = $vendeur = "";
+$chaine = "";
 if (isset($_POST["titre"], $_POST["description"], $_POST["prix"], $_POST["negociable"], $_POST["vendeur"])) {
     $titre = $_POST["titre"];
     $description = $_POST["description"];
     $prix = $_POST["prix"];
     $negociable = $_POST["negociable"];
-    isset($_POST["image"]) ? $image = $_POST["image"] : $image = "https://media.istockphoto.com/id/1415203156/vector/error-page-page-not-found-vector-icon-in-line-style-design-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=RuQ_sn-RjAVNKOmARuSf1oXFkVn3OMKeqO5vw8GYoS8=";
     $vendeur = $_POST["vendeur"];
+    if (isset($_POST["image"]) && !empty($_POST["image"])) {
+        $image = $_POST["image"];
+        //ENVOYER AU CSV AVEC IMAGE
+        $chaine = $titre . ";" . $description . ";" . $prix . ";" . $negociable . ";" . $image . ";" . $vendeur . "\n";
+        fwrite($fichier, $chaine);
+    } else {
+        //ENVOYER AU CSV SANS IMAGE
+        $chaine = $titre . ";" . $description . ";" . $prix . ";" . $negociable . ";" . $vendeur . "\n";
+        fwrite($fichier, $chaine);
+    }
 
-    // Envoyer les données dans le fichier csv
-
-    $chaine = $titre . ";" . $description . ";" . $prix . ";" . $negociable . ";" . $image . ";" . $vendeur . "\n";
-    fwrite($fichier, $chaine);
     echo "Données enregistrées avec succès. Merci d'avoir placé votre confiance en Abdou's Market.";
 } else
     echo "Erreur: Tous les champs doivent être rempli!";
 
 fclose($fichier);
-?>
